26480f2ff5ee755a9bce60e69820e811a2e775ff
Asad Ali Khan WL1 <asad.khan@cooperativecomputing.com>
2023-03-09 12:43:35 +0500

    added db etc also modified controller and error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,28 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const port = process.env.PORT || 5000
+const express = require("express");
 
-const app = express()
+const colors = require("colors");
+const connectDb = require("./config/db");
+const path = require("path");
+const dotenv = require("dotenv").config({
+  path: path.resolve(__dirname, "../.env"),
+});
+const { errorHandler } = require("./middleware/errorMiddleware");
+const port = process.env.PORT || 5000;
 
+connectDb();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-//  app.get('/api/goals',(req,res) => { 
+//  app.get('/api/goals',(req,res) => {
 //     res.status(200).json({message:'GETTING GOALS YOU BITCH'})
 
 //   })
 
-app.use("/api/goals",require('./routes/goalRoutes'))
+app.use("/api/goals", require("./routes/goalRoutes"));
 
+app.use(errorHandler);
 
-
-
-app.listen(port,() => { 
-   return console.log('server started on port ' + port)
- })
+app.listen(port, () => {
+  return console.log("server started on port " + port);
+});
